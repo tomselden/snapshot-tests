61b3baee98916de8aa9f5eea0d82f39da9e6fd26
Thomas Selden <thomasselden42@gmail.com>
2022-02-11 15:07:22 -0500

    added snapshot tests for app.js and GitHubCard.js

---
+++ src/components/GitHubCard.js
@@ -0,0 +1,16 @@
+import { Card } from "react-bootstrap";
+
+const GitHubCard = function () {
+  return (
+    <Card>
+      <h1>tomselden</h1>
+      <img></img>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </Card>
+  );
+};
+
+export default GitHubCard;
--- src/components/GitHubCard.test.js
+++ src/components/GitHubCard.test.js
@@ -1,7 +1,7 @@
-import App from "./App";
+import GitHubCard from "./GitHubCard";
 import renderer from "react-test-renderer";
 
 test("renders a snapshot", () => {
-  const tree = renderer.create(<App />).toJSON();
+  const tree = renderer.create(<GitHubCard />).toJSON();
   expect(tree).toMatchSnapshot();
 });
